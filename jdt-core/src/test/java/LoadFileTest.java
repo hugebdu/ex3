583b3cf9f3ef4b32361a1b8d9285d9f286b8cb41
alexanderva <alexanderva@gmail.com>
2012-05-12 14:57:59 +0300

    Add time calculation test

--- jdt-core/src/test/java/LoadFileTest.java
+++ jdt-core/src/test/java/LoadFileTest.java
@@ -21,4 +21,28 @@ public class LoadFileTest
             System.out.println(triangle_dt);
         }
     }
+
+    @Test
+    public void testCalcTime() throws Exception
+    {
+        int NUMBUR_OF_TESTS = 100;
+        String file = getClass().getResource("terra_13000.tsin").getFile();
+        long[] times = new long[NUMBUR_OF_TESTS];
+        long totalTime = 0;
+
+        for (int i = 0;i<NUMBUR_OF_TESTS;i++)
+        {
+            long startTimeMs = System.currentTimeMillis();
+            Delaunay_Triangulation triangulation = new Delaunay_Triangulation(file);
+            times[i]=System.currentTimeMillis() - startTimeMs;
+            totalTime+=times[i];
+        }
+
+        System.out.println(String.format("Average processing time of %s iterations is %s ms", NUMBUR_OF_TESTS, totalTime / NUMBUR_OF_TESTS));
+        for (int i = 0;i<NUMBUR_OF_TESTS;i++)
+        {
+            System.out.println(String.format("   - iteration #%s time is %s ms", i+1, times[i]));
+        }
+
+    }
 }
